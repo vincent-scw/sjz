1b17d12915e9d81513276624a751939185f8c93d
Vincent Shen <edentidus@foxmail.com>
2020-04-10 01:07:22 +0000

    add return url

--- server/SJZ.Server/SJZ.OAuthService/wwwroot/js/auth.js
+++ server/SJZ.Server/SJZ.OAuthService/wwwroot/js/auth.js
@@ -1,3 +1,6 @@
 ﻿const callAuthProvider = (provider) => {
-    window.location.href = 'External/' + provider;
+    const urlParams = new URLSearchParams(window.location.search);
+    const redirect_uri = urlParams.get('redirect_uri');
+    console.log(redirect_uri)
+    window.location.href = `External/${provider}?returnUrl=${redirect_uri}`;
 }

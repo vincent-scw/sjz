842b8fa628407c9aa42c461998f8ec2491d8f3d2
Vincent Shen <edentidus@foxmail.com>
2020-04-28 00:38:23 +0000

    add function

--- client/src/app/areas/timeline/action-panel/action-panel.component.ts
+++ client/src/app/areas/timeline/action-panel/action-panel.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-action-panel',
   templateUrl: './action-panel.component.html'
 })
 export class ActionPanelComponent {
-
+  @Output() add = new EventEmitter<void>();
+  @Output() edit = new EventEmitter<void>();
+  @Output() delete = new EventEmitter<void>();
 }

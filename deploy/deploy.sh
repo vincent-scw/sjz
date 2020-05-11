dbc5a639c7091b72074f305892c7617ba3813e5a
Vincent Shen <edentidus@foxmail.com>
2020-05-11 08:59:06 +0000

    add some deploy scripts

---
+++ deploy/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VERSION_NUMBER=$1
+echo "Travis version number is "$VERSION_NUMBER
+
+for f in ./deploy/sjz/*.yaml
+do
+  template=`cat $f | sed "s/{{VERSION_NUMBER}}/$VERSION_NUMBER/g"`
+  echo "$template" | kubectl apply -f -
+done
